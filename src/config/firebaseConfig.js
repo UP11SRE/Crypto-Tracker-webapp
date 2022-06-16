9a1bd016cd14ce986fa6899fe53b11660bb63d0e
Naman <naman@iiitb.online>
2022-06-17 00:19:29 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyS8l8vODTuJTTBASEqdwE38-VCRp3kVs",
+  authDomain: "crypto-tracker-f0f8f.firebaseapp.com",
+  projectId: "crypto-tracker-f0f8f",
+  storageBucket: "crypto-tracker-f0f8f.appspot.com",
+  messagingSenderId: "842188678832",
+  appId: "1:842188678832:web:90ba6ce9356b559283fd75"
+};
+
+export default firebaseConfig;
