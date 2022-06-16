4f7d4d104bfcf7dc2f2441a845c325076e7741f5
NAMAN KANSAL <95614570+UP11SRE@users.noreply.github.com>
2022-06-17 01:25:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { makeStyles } from "@material-ui/core";
-import Homepage from "./Pages/HomePage";
+//import Homepage from "./Pages/HomePage";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-import CoinPage from "./Pages/CoinPage";
+//import CoinPage from "./Pages/CoinPage";
+import { lazy , Suspense} from "react";
+
 import Header from "./components/Header";
 import Alert from "./components/Alert";
+
+const Homepage = lazy(() => import("./Pages/HomePage"));
+const CoinPage = lazy(() => import("./Pages/CoinPage"));
 
 const useStyles = makeStyles(() => ({
   App: {
@@ -21,8 +26,10 @@ function App() {
     <BrowserRouter>
       <div className={classes.App}>
         <Header />
+        <Suspense fallback={<div>Loading...</div>}>
         <Route path="/" component={Homepage} exact />
         <Route path="/coins/:id" component={CoinPage} exact />
+        </Suspense>
       </div>
       <Alert />
     </BrowserRouter>
